3ce4bb1faa540945adca9126064d6858bc886fae
Amit Jha <amitjha4321@gmail.com>
2020-12-06 00:19:57 +0545

    backend started, homepage layout done

--- app/src/main/java/com/example/loginapplication/MainActivity.java
+++ app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -3,8 +3,26 @@ package com.example.loginapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+    private TextView eAttemptsInfo;
+
+    private String Username="Admin";
+    private String Password="12345678";
+
+    private boolean isValid= false;
+
+    private int counter=5;
+
 
 
 
@@ -16,5 +34,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eName= findViewById(R.id.etName);
+        ePassword= findViewById(R.id.etPassword);
+        eLogin=findViewById(R.id.btnLogin);
+        eAttemptsInfo=findViewById(R.id.tvAttemptsInfo);
+        //set an onclick listener on the button
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName=eName.getText().toString();
+                String inputPassword= ePassword.getText().toString();
+                if (inputName.isEmpty() || inputPassword.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Please Enter all The Details Correctly!",Toast.LENGTH_LONG).show();
+                }else{
+                    isValid=validate(inputName,inputPassword);
+                    if (!isValid){
+                        counter--;
+                        Toast.makeText(MainActivity.this,"Incorrect Credentials!",Toast.LENGTH_LONG).show();
+                        if (counter==0){
+                            eLogin.setEnabled(false);
+                        }
+                    }else {
+                        Toast.makeText(MainActivity.this,"Login successful!",Toast.LENGTH_LONG).show();
+                        //add the code to go to the homepage/other activity page
+                    }
+                }
+            }
+        });
+    }
+    private boolean validate(String name, String password){
+        if (name.equals(Username) && password.equals(Password)){
+            return true;
+        }
+        return false;
     }
 }
